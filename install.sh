f876ab0862486415b60cc054f21814110281041c
Aman Singh Katal <besoeasy@users.noreply.github.com>
2023-11-22 11:24:27 +0530

    1.2.1

--- install.sh
+++ install.sh
@@ -25,8 +25,24 @@ if command -v telepi &> /dev/null; then
 else
     # Telepi is not installed, proceed with installation
 
-    curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
-    sudo apt-get install -y nodejs
+
+# Update system and install dependencies
+apt-get update -y
+apt-get install -y ca-certificates curl gnupg
+
+# Install Node.js from NodeSource repository
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=20
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
+
+# Update and install Node.js
+apt-get update && apt-get install nodejs -y
+
+# Check Node.js version
+node --version
+
+echo "Node.js installation complete!"
+
 
     # Prompt user for TELEGRAMBOT variable with regex check
     while true; do
