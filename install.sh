bf820822e7a3f77cdb63dd7e3109eab73e733700
besoeasy <besoeasy@users.noreply.github.com>
2023-11-22 11:37:39 +0530

    update install.sh

--- install.sh
+++ install.sh
@@ -8,10 +8,18 @@ if [ "$EUID" -ne 0 ]; then
 fi
 
 sudo apt-get update -y
-sudo apt-get install aria2 -y
+sudo apt-get install -y ca-certificates curl gnupg aria2
 
 # Check if telepi is installed
 if command -v telepi &> /dev/null; then
+
+    # Check if npm is installed
+    if ! command -v npm &> /dev/null; then
+        echo "npm is not installed. Please install npm first."
+        exit 1
+    fi
+
+
     # Telepi is installed, stop the service
     sudo systemctl stop telepi.service
 
@@ -27,8 +35,7 @@ else
 
 
 # Update system and install dependencies
-apt-get update -y
-apt-get install -y ca-certificates curl gnupg
+sudo apt-get update -y
 
 # Install Node.js from NodeSource repository
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
@@ -36,7 +43,7 @@ NODE_MAJOR=20
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
 
 # Update and install Node.js
-apt-get update && apt-get install nodejs -y
+sudo apt-get update && apt-get install nodejs -y
 
 # Check Node.js version
 node --version
