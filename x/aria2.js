15c14ad6001eab6c61ea531ddc7a740cef75e36c
Aman Singh Katal <besoeasy@users.noreply.github.com>
2024-07-31 18:48:59 +0530

    --

--- x/aria2.js
+++ x/aria2.js
@@ -1,5 +1,15 @@
 const axios = require("axios");
 
+async function getIpData() {
+  try {
+      const response = await axios.get('http://ip-api.com/json/');
+      return response.data;
+  } catch (error) {
+      console.error('Error fetching data:', error);
+      return null;
+  }
+}
+
 const path = require("path");
 
 const tempdir = require("os").tmpdir();
