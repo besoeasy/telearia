ce1933bbce0abd262dd55c54c3306fab99bb136e
besoeasy <besoeasy@users.noreply.github.com>
2024-08-02 23:16:45 +0530

    stable

--- docker.sh
+++ docker.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Start aria2c in the background
-aria2c --enable-rpc --rpc-listen-all --rpc-listen-port=6800 --enable-dht --dht-listen-port=6881-6888 --seed-time=2000 &
+aria2c --enable-rpc --rpc-listen-all --rpc-listen-port=6800 --enable-dht --dht-listen-port=6881-6888 --seed-time=50 &
 
 # Start the Node.js application
 exec node index.js
