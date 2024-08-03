af1b283f8f6baa4cb789160bf28c48b774796460
Aman Singh Katal <besoeasy@users.noreply.github.com>
2024-08-03 23:56:28 +0530

    Update utils.js

--- func/utils.js
+++ func/utils.js
@@ -11,7 +11,28 @@ const tempdir = require("os").tmpdir();
 
 const saveDirectory = path.join(tempdir, "downloads");
 
+function deleteOldFiles(days = 30 ) {
+
+  const command = `find "${saveDirectory}" -type f -mtime +${days} -exec rm {} \\;`;
+
+  exec(command, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error executing command: ${error.message}`);
+      return;
+    }
+
+    if (stderr) {
+      console.error(`stderr: ${stderr}`);
+      return;
+    }
+
+    console.log(`stdout: ${stdout}`);
+    console.log('Old files deleted successfully.');
+  });
+}
+
 module.exports = {
   bytesToSize,
   saveDirectory,
+  deleteOldFiles
 };
