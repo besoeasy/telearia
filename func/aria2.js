b310c84ba6773a7ab3ccff8f56181266f9bf18da
Aman Singh Katal <besoeasy@users.noreply.github.com>
2024-08-31 01:43:31 +0530

    --

--- func/aria2.js
+++ func/aria2.js
@@ -4,18 +4,6 @@ const path = require("path");
 
 const { saveDirectory } = require("./utils.js");
 
-function getTodaysDate() {
-  const today = new Date();
-
-  const day = String(today.getDate()).padStart(2, "0");
-  const month = today
-    .toLocaleString("default", { month: "short" })
-    .toUpperCase();
-  const year = today.getFullYear();
-
-  return `${day}-${month}-${year}`;
-}
-
 const axiosPost = async (method, params = []) => {
   const { data } = await axios.post("http://localhost:6800/jsonrpc", {
     jsonrpc: "2.0",
@@ -36,7 +24,6 @@ const getGlobalStats = async () => {
 };
 
 const downloadAria = async (id, url) => {
-
   return await axiosPost("aria2.addUri", [
     [url],
     {
