5a5152363a4ae501d2004bc5fdcccdfbc1bec62b
besoeasy <besoeasy@users.noreply.github.com>
2024-11-22 20:42:28 +0530

    ip updated

--- func/ip.js
+++ func/ip.js
@@ -2,8 +2,8 @@ const axios = require("axios");
 
 async function getIpData() {
   try {
-    const response = await axios.get("http://ip-api.com/json/");
-    return response.data;
+    const { data } = await axios.get("http://ip-api.com/json/");
+    return data;
   } catch (error) {
     console.error("Error fetching data:", error);
     return null;
