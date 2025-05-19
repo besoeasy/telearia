81f51637954c47200db572b41d25713e16e62825
besoeasy <besoeasy@users.noreply.github.com>
2025-05-19 22:44:39 +0530

    fix: reorder aria2c command execution for improved script flow

--- start.sh
+++ start.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
-aria2c \
-  --enable-rpc \
-  --rpc-listen-all \
-  --rpc-listen-port=6398 \
-  --enable-dht \
-  --enable-peer-exchange=true \
-  --bt-tracker='udp://tracker.opentrackr.org:1337/announce,udp://opentracker.io:80/announce,udp://tracker.torrent.eu.org:451/announce,udp://tracker.internetwarriors.net:1337/announce,udp://tracker.leechers-paradise.org:6969/announce,udp://tracker.coppersurfer.tk:6969/announce,udp://tracker.cyberia.is:6969/announce,udp://exodus.desync.com:6969/announce,udp://explodie.org:6969/announce,udp://open.stealth.si:80/announce,udp://tracker.moeking.me:6969/announce,udp://tracker.openbittorrent.com:80/announce' \
-  --seed-time=100 &
-
-sleep 2
-
 # Ensure SAVE_DIR exists
 SAVE_DIR=$(node -e "console.log(require('os').tmpdir() + '/telearia')")
 
 mkdir -p "$SAVE_DIR"
 
 echo "Download directory: $SAVE_DIR"
+
+sleep 2
+
+# Start Aria2c
+aria2c \
+  --enable-rpc \
+  --rpc-listen-all \
+  --rpc-listen-port=6398 \
+  --bt-tracker='udp://tracker.opentrackr.org:1337/announce,udp://opentracker.io:80/announce,udp://tracker.torrent.eu.org:451/announce,udp://tracker.internetwarriors.net:1337/announce,udp://tracker.leechers-paradise.org:6969/announce,udp://tracker.coppersurfer.tk:6969/announce,udp://tracker.cyberia.is:6969/announce,udp://exodus.desync.com:6969/announce,udp://explodie.org:6969/announce,udp://open.stealth.si:80/announce,udp://tracker.moeking.me:6969/announce,udp://tracker.openbittorrent.com:80/announce' \
+  --seed-time=100 &
 
 sleep 2
 
